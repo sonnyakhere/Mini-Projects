3d85ec6a12880e554253415e634174e960564a70
Akhere Sonny-Egbeahie <sonnydaniel12@gmail.com>
2022-02-16 00:44:53 +0100

    working

--- Password Checker.py
+++ Password Checker.py
@@ -1,5 +1,31 @@
 import re
 
-passwd = input('Input a Password:')
+def response():
+    print('Password must be atlease 6-20 characters long, contain uppercase, lowercase, a number and a symbol, try again.')
 
-print(passwd)
+def request():
+    global passwd
+    passwd = input('Input a Password:')
+
+
+while True:
+    request()
+    if len(passwd)<6 or len(passwd)>20:
+        response()
+        
+    elif not re.search('[a-z]', passwd):
+        response()
+        
+    elif not re.search('[A-Z]', passwd):
+        response()
+        
+    elif not re.search('[_$@#-]', passwd):
+        response()
+        
+    elif re.search('[ ]', passwd):
+        print('Spaces are not allowed, try again.')
+        
+    else:
+        print('Valid Password.')
+        break
+
