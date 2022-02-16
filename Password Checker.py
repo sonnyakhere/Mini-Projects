fefa5ecc7d94ce8adf369c4d281d8af521c7f31a
Akhere Sonny-Egbeahie <sonnydaniel12@gmail.com>
2022-02-16 14:07:02 +0100

    update

--- Password Checker.py
+++ Password Checker.py
@@ -3,13 +3,9 @@ import re
 def response():
     print('Password must be atlease 6-20 characters long, contain uppercase, lowercase, a number and a symbol, try again.')
 
-def request():
-    global passwd
-    passwd = input('Input a Password:')
-
 
 while True:
-    request()
+    passwd = input('Input a Password:')
     if len(passwd)<6 or len(passwd)>20:
         response()
         
